a552b14f358f793015dc9aec1db55da8f9be27c4
Valeria Castillo <valeriacastillo415@gmail.com>
2021-06-28 13:42:20 -0700

    fix calx

--- src/form/form.component.jsx
+++ src/form/form.component.jsx
@@ -1,6 +1,9 @@
 /* eslint-disable no-unused-vars */
 import "./form.styles.scss";
 import { useState } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+import { css } from "@emotion/react";
+
 import validateInput from "./validators/input-validator.component";
 import validateForm from "./validators/form-validator.component";
 import { Display } from "../display/display.component";
@@ -13,7 +16,13 @@ export const Form = ({ addValues }) => {
   const [shortestPath, setShortestPath] = useState();
   const [error, setErrors] = useState({});
   const [disabled, setDisabled] = useState(true);
-  const [isLoading, setLoading] = useState(false);
+
+  /** SPINNER STYLE */
+  const spinner = css`
+    display: block;
+    margin: 0 auto;
+    border-color: whitesmoke;
+  `;
 
   /** HANDLE CHANGE */
   const handleChange = (e) => {
@@ -63,9 +72,8 @@ export const Form = ({ addValues }) => {
       return;
     } else {
       addValues([bucketA, bucketB, bucketGoal]);
-
       // Find path
-      const shortestPath = getShortestPath(bucketA, bucketB, bucketGoal);
+      getShortestPath(bucketA, bucketB, bucketGoal);
 
       setDefaults();
     }
@@ -79,97 +87,79 @@ export const Form = ({ addValues }) => {
   };
 
   const getShortestPath = (maxSmBucket, maxLgBucket, bucketGoal) => {
-    setLoading(true);
     /** Breadth-First Search: BFS will not lead to an infinite loop and finds the shortest possible path between the root node and the goal via other accessible nodes. */
     /** STEPS 
-      - Step One: Fill the m litre jug and empty its contents into the n litre jug.
-      - Step Two: If at any instant the m litre jug becomes empty fill it with water.
-      - Step Three: If at any instant the n litre jug becomes full empty it.
-      - Step Four: Repeat steps one, two, and three till any of the jugs among the n litre jug and the m litre jug contains exactly d litres of water.
+      - Step One: Fill the large bucket and transfer its contents into the small bucket.
+      - Step Two: If at any instant large bucket becomes empty fill it with water.
+      - Step Three: If at any instant the small bucket becomes full empty it.
+      - Step Four: Repeat steps one, two, and three until any of the buckets contains exactly the goal amount of water. */
 
-    The time complexity of the solutions is usually O (m*n). */
-
-    // FILL - fill bucket to capacity
     const fillBucket = (buckets, key = "large", max = maxLgBucket) => ({
       ...buckets,
       [key]: max,
     });
 
-    // DUMP - set state back to 0
     const dumpBucket = (buckets, key = "large") => ({ ...buckets, [key]: 0 });
 
-    // TRANSFER LARGE => SMALL
     const lgToSm = ({ large, small }) => {
-      const quantityNeededToFillSmBucket = maxSmBucket - small;
+      const quantityNeededToFillSmall = maxSmBucket - small;
 
       return {
         large:
-          large > quantityNeededToFillSmBucket
-            ? large - quantityNeededToFillSmBucket
+          large > quantityNeededToFillSmall
+            ? large - quantityNeededToFillSmall
             : 0,
         small:
-          large > quantityNeededToFillSmBucket
-            ? small + quantityNeededToFillSmBucket
+          large > quantityNeededToFillSmall
+            ? small + quantityNeededToFillSmall
             : small + large,
       };
     };
 
-    // TRANSFER SMALL => LARGE
-    const smToLg = ({ small, large }) => {
-      const quantityNeededToFillLgBucket = maxLgBucket - large;
+    const smToLg = ({ large, small }) => {
+      const quantityNeededToFillLarge = maxLgBucket - large;
 
       return {
         large:
-          small > quantityNeededToFillLgBucket
-            ? (small = quantityNeededToFillLgBucket)
+          small > quantityNeededToFillLarge
+            ? small - quantityNeededToFillLarge
             : 0,
         small:
-          small > quantityNeededToFillLgBucket
-            ? large + quantityNeededToFillLgBucket
+          small > quantityNeededToFillLarge
+            ? large + quantityNeededToFillLarge
             : small + large,
       };
     };
 
-    // Checks if new state already exists in the path
-    const isPairRepeated = (path, { small, large }) =>
+    const isRepeated = (path, { small, large }) =>
       !!path.find((x) => x.small === small && x.large === large);
 
-    /** BEGIN CALCULATIONS */
     const queue = [];
     const path = [];
 
-    path.push(bucketState); // Initial state of buckets (0, 0)
-    queue.push(path); // Start queue with initial bucket state
+    path.push(bucketState);
+    queue.push(path);
 
     while (queue.length) {
-      const lastPath = queue.shift(); // first element in queue is saved
+      const lastPath = queue.shift();
       const lastState = lastPath[lastPath.length - 1];
 
-      if (bucketGoal === lastState.large) {
-        setLoading(false);
-        setShortestPath(lastPath); // last path pair = end search for shortest path
-      }
+      if (bucketGoal === lastState.large) return setShortestPath(lastPath);
 
-      // create set using the lastState
       const states = new Set([
-        fillBucket(lastState), // fill large
-        fillBucket(lastState, "small", maxSmBucket), // fill small
-        lgToSm(lastState), // transfer lg to sm
-        smToLg(lastState), // transfer sm to lg
-        dumpBucket(lastState), // dump large bucket
-        dumpBucket(lastState, "small"), // dump small bucket
+        fillBucket(lastState),
+        fillBucket(lastState, "small", maxSmBucket),
+        lgToSm(lastState),
+        smToLg(lastState),
+        dumpBucket(lastState),
+        dumpBucket(lastState, "small"),
       ]);
 
-      // loop through states and check for repeated pairs
-      // create the queue and return with shortest path
       for (let item of states) {
-        if (!isPairRepeated(lastPath, item)) {
+        if (!isRepeated(lastPath, item)) {
           const newPath = [...lastPath];
           newPath.push(item);
           queue.push(newPath);
-
-          setLoading(false);
-          setShortestPath(queue);
         }
       }
     }
@@ -247,13 +237,10 @@ export const Form = ({ addValues }) => {
           title="Calculate"
           disabled={disabled}
         >
-          {isLoading ? (
-            <ClipLoader size={15} css={spinner} margin={2} />
-          ) : (
-            "Calculate"
-          )}
+          Calculate
         </button>
       </form>
+
       {shortestPath && <Display shortestPath={shortestPath}></Display>}
     </div>
   );
